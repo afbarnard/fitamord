032d55b0636660f4bcecdffa9da8db792bff397d
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2017-04-13 10:51:53 -0500

    Fixed and improved `Field` and `Header`.
    * Tested and fixed above classes by hand.
    * Improved `Header` constructor to interpret positional arguments based
      on their type / content.
    * Added dictionaries and keyword arguments as additional ways to specify
      (name, type) pairs when constructing a `Header`.  These will be more
      useful when running Python 3.6 which introduced order preservation for
      keyword arguments and a compact `dict` representation which _happens_
      to preserve insertion order.
    * Added error handling to `Header` constructor.
    * Added `__getitem__` method to `Header` to allow indexing fields by
      index or name.  Having `__len__` and `__getitem__` enables iteration
      as well.
    * Fixed `Header.isinstance` to not accept records shorter than the
      header.
    * Implemented `__repr__` methods to aid inspection and debugging.
    * Updated copyright year.

--- fitamord/records.py
+++ fitamord/records.py
@@ -1,8 +1,10 @@
 """Core record processing
 
 """
-# Copyright (c) 2016 Aubrey Barnard.  This is free software released
+# Copyright (c) 2017 Aubrey Barnard.  This is free software released
 # under the MIT License.  See `LICENSE.txt` for details.
+
+import itertools as itools
 
 from . import general
 
@@ -30,33 +32,81 @@ class Field:
     def isinstance(self, obj):
         return isinstance(obj, self._type)
 
-    def __repr__(self): # TODO for debugging
-        return super().__repr__()
+    def __repr__(self):
+        return 'Field(name={!r}, typ={})'.format(
+            self.name, self.type.__name__)
 
 
 class Header:
-    """A description of a collection of fields where each field has a name
+    """A definition of a collection of fields where each field has a name
     and a type.
 
     The type of a record or the element type of a collection of records
-    or a table.
+    (such as a table).
 
     """
 
-    def __init__(self, names=None, types=None, fields=None):
-        # TODO make constructable from an iterable of names or a dictionary
+    def __init__(self, fields=None, types=None, **names_to_types):
+        """Create a header from the given fields.
+
+        The fields can be given as an interable of `Field`s, an iterable
+        of (name, type) pairs, both an iterable of names and a
+        corresponding iterable of types, just an iterable of names
+        (assumes types are all `object`), a mapping of names to types,
+        or name=type keyword arguments.
+
+        """
+        # TODO allow fields / types to be strings?
+        # Convert arguments to a standard form
+        field_defs = ()
+        # Parallel iterables of names and types
+        if fields is not None and types is not None:
+            field_defs = zip(fields, types)
+        # Mapping of names to types
+        elif isinstance(fields, dict):
+            field_defs = fields.items()
+        # Iterable of field definitions
+        elif fields is not None:
+            field_defs = fields
+        # Include keyword arguments if any
+        if names_to_types:
+            if field_defs:
+                field_defs = itools.chain(
+                    field_defs, names_to_types.items())
+            else:
+                field_defs = names_to_types.items()
+
+        # Members: list of fields, their indices
         self._fields = []
         self._names2idxs = {}
-        for name, typ in zip(names, types):
-            self._names2idxs[name] = len(self._fields)
-            self._fields.append(Field(name, typ))
-        for field in fields:
-            if not isinstance(field, Field):
-                # Unpack iterable (treat as a pair)
-                name, typ, *_ = field
-                field = Field(name, typ)
+        # Process each field definition to construct a field
+        for field_def in field_defs:
+            field = None
+            # `Field`
+            if isinstance(field_def, Field):
+                field = field_def
+            # Name
+            elif isinstance(field_def, str):
+                field = Field(field_def)
+            # (name, type) pair
+            elif len(field_def) == 2:
+                field = Field(*field_def)
+            # Otherwise error
+            else:
+                raise ValueError(
+                    'Could not interpret as a field: {!r}'
+                    .format(field_def))
+            # Duplicate name is an error
+            if field.name in self._names2idxs:
+                raise ValueError(
+                    'Duplicate field name: {!r}'.format(field.name))
+            # Update members
             self._names2idxs[field.name] = len(self._fields)
             self._fields.append(field)
+
+        # Check that at least one field was specified
+        if not self._fields:
+            raise ValueError('No fields were specified')
 
     def __len__(self):
         return len(self._fields)
@@ -72,6 +122,11 @@ class Header:
 
     def field_at(self, index):
         return self._fields[index]
+
+    def __getitem__(self, index):
+        if isinstance(index, str):
+            return self.field_of(index)
+        return self.field_at(index)
 
     def name_at(self, index):
         return self._fields[index].name
@@ -89,17 +144,22 @@ class Header:
         return self._names2idxs[name]
 
     def isinstance(self, record):
+        idx = 0
         for idx, value in enumerate(record):
-            if (idx >= len(self._fields)
-                or not self._fields[idx].isinstance(value)):
+            if (idx >= len(self)
+                    or not self._fields[idx].isinstance(value)):
                 return False
+        # Check that the length of the record equals that of the header
+        if idx != len(self) - 1:
+            return False
         return True
 
     def items(self): # TODO for writing config or making dict
         return ()
 
-    def __repr__(self): # TODO for debugging
-        return super().__repr__()
+    def __repr__(self):
+        return ('Header((' + ', '.join(repr(f) for f in self._fields)
+                + '))')
 
 
 class Record:
